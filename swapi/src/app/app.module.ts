1a8cd53198447f7326027bba0a06374cf8382887
Mostafa Anwar <mostafa.anwar12437@hotmail.com>
2021-01-30 19:13:40 +0200

    added new component

--- swapi/src/app/app.module.ts
+++ swapi/src/app/app.module.ts
@@ -4,13 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { PersonsComponent } from './persons/persons.component';
-import { PersonInputComponent } from './persons/person-input.component';
+import { PersonsComponent } from './components/persons/persons.component';
+import { PersonInputComponent } from './components/persons/person-input.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
-  declarations: [AppComponent, PersonsComponent, PersonInputComponent],
+  declarations: [AppComponent, PersonsComponent, PersonInputComponent, SearchComponent],
   imports: [BrowserModule, FormsModule, HttpClientModule, AppRoutingModule],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
